aa75b37a8b3b386cc562e7b515fa072f505edfa5
unknown <tamiloreolaniyi@gmail.com>
2022-08-30 22:43:33 +0100

    slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,153 @@
-import React from 'react'
+import {ArrowLeftOutlined, ArrowRightOutlined} from '@material-ui/icons';
+import React from 'react';
+import styled from 'styled-components';
+import {useState} from 'react';
+
+const Container = styled.div`
+    width: 100%;
+    height:100vh;
+    display:flex; 
+    position: relative;
+    overflow: hidden;  
+`;
+
+const Wrapper = styled.div`
+    height: 100px;
+    display: flex;
+    transform: translateX(0vw);
+`;
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff7f7;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction === 'left' && '10px'};
+    right: ${props => props.direction === 'right' && '10px'};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
+    z-index: 2;
+
+`;
+
+const Slide = styled.div`
+    width: 100wv;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color: #${props => props.bg};
+`;
+
+const ImgContainer = styled.div`
+    flex: 1;
+`;
+
+const Image = styled.img`
+    height: 80%;
+`;
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`;
+
+const Title = styled.h1`
+    font-size: 70px;
+`;
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background: transparent;
+    cursor: pointer;
+`;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState (0);
+  const handleClick = direction => {};
   return (
-    <div>Slider</div>
-  )
-}
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick ('left')}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fcf1ed">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>WINTER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fbf0f4">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SPRING SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>AUTUMN SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick ('right')}>
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
 
-export default Slider
+export default Slider;
