fc0a962f8eac5b2ff34c33032c8b2632653e92e6
unknown <tamiloreolaniyi@gmail.com>
2022-08-30 23:28:20 +0100

    slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,84 +5,95 @@ import {useState} from 'react';
 import {sliderItems} from './data';
 
 const Container = styled.div`
-    width: 100%;
-    height:100vh;
-    display:flex; 
-    position: relative;
-    overflow: hidden;  
-`;
-
-const Wrapper = styled.div`
-    height: 100px;
-    display: flex;
-    transform: translateX(0vw);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
-    width: 50px;
-    height: 50px;
-    background-color: #fff7f7;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: ${props => props.direction === 'left' && '10px'};
-    right: ${props => props.direction === 'right' && '10px'};
-    margin: auto;
-    cursor: pointer;
-    opacity: 0.5;
-    z-index: 2;
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
 
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
-    width: 100wv;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    background-color: #${props => props.bg};
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
-    flex: 1;
+  height: 100%;
+  flex: 1;
 `;
 
 const Image = styled.img`
-    height: 80%;
+  height: 80%;
 `;
+
 const InfoContainer = styled.div`
-    flex: 1;
-    padding: 50px;
+  flex: 1;
+  padding: 50px;
 `;
 
 const Title = styled.h1`
-    font-size: 70px;
+  font-size: 70px;
 `;
+
 const Desc = styled.p`
-    margin: 50px 0px;
-    font-size: 20px;
-    font-weight: 500;
-    letter-spacing: 3px;
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
 `;
+
 const Button = styled.button`
-    padding: 10px;
-    font-size: 20px;
-    background: transparent;
-    cursor: pointer;
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState (0);
-  const handleClick = direction => {};
+  const handleClick = direction => {
+
+    if(direction === 'left') {
+        setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2)
+    } else {
+        setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0)
+    }
+  };
   return (
     <Container>
       <Arrow direction="left" onClick={() => handleClick ('left')}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper>
+      <Wrapper slideIndex ={slideIndex} >
         {sliderItems.map (item => (
           <Slide bg={item.bg}>
             <ImgContainer>
