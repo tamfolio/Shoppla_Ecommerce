c997b4ddb5e61dd3c103371fdf503dbc4a6fff34
unknown <tamiloreolaniyi@gmail.com>
2022-09-01 23:56:24 +0100

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Product from "./pages/Product";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return <div>
-    <Home/>
+    <Cart/>
   </div>;
 };
 
