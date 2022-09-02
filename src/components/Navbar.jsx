ce863a51e170995797ad548879c14a7861322036
unknown <tamiloreolaniyi@gmail.com>
2022-09-02 22:17:49 +0100

    adding responsive css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,18 @@ import {Search, ShoppingCartOutlined} from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 import { Badge } from '@material-ui/core';
+import {mobile} from '../responsive';
 
 const Container = styled.div`
     height: 60px;
+    ${mobile ({ height: '50px'})}
 `;
 
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
     justify-content: space-between;
+    ${mobile({ padding: '10px 0px'})}
 `;
 
 const Left = styled.div`
@@ -23,16 +26,19 @@ const Left = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: '50px'})}
 `
 
 const Logo = styled.h1`
-
   font-weight: bold;
+  ${mobile({ fontSize: '24px'})}
 `
 
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
+    ${mobile({ display: 'none' })}
+
 `;
 const Center = styled.div`
     flex: 1;
@@ -43,6 +49,7 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile({ justifyContent: 'center'})}
 `;
 
 const SearchContainer = styled.div`
@@ -57,6 +64,7 @@ const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: '12px', marginLeft: '10px'})}
 `
 
 const Navbar = () => {
@@ -68,7 +76,7 @@ const Navbar = () => {
             EN
           </Language>
           <SearchContainer>
-            <Input/>
+            <Input placeholder='Search'/>
             <Search  style={{ color:'gray', fontSize: 16}}/>
           </SearchContainer>
         </Left>
