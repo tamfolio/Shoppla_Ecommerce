1ecdf036d3394cc7d6660ef15c98bbafc11ef68b
unknown <tamiloreolaniyi@gmail.com>
2022-09-02 22:03:47 +0100

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Product from "./pages/Product";
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Cart from "./pages/Cart";
+import Product from './pages/Product';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
 
 const App = () => {
-  return <div>
-    <Cart/>
-  </div>;
+  return (
+    <div>
+      <Cart />
+    </div>
+  );
 };
 
 export default App;
