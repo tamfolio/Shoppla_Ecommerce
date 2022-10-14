8b33a43623ba60bf5e09424188add9375183af82
unknown <tamiloreolaniyi@gmail.com>
2022-10-14 02:13:18 +0100

    routing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter,Routes,Route, } from "react-router-dom";
 import App from './App';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Register from './pages/Register';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="cart" element={<Cart/>} />
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
