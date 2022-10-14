9df20a8eadb7bbdc89cea0a5c746dfbe5368d318
unknown <tamiloreolaniyi@gmail.com>
2022-10-14 02:13:18 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import Home from './pages/Home';
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
+  const user = true;
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/products/:category">
+        <ProductList />
+      </Route>
+      <Route path="/product/:id">
+        <Product />
+      </Route>
+      <Route path="/cart">
+        <Cart />
+      </Route>
+      <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+      <Route path="/register">
+        {user ? <Redirect to="/" /> : <Register />}
+      </Route>
+    </Switch>
+  </Router>
   );
 };
 
